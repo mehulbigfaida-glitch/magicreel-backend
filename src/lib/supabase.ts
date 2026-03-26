d8b6a7d7657885aee73b56fcd8e0fc9af81ce884
Mehul Gandhi <mehul.bigfaida@gmail.com>
2026-03-26 06:00:24 +0000

    fix supabase service role

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_ANON_KEY!;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseServiceKey
+);
