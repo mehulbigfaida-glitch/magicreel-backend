48c75014fadb078654e570ea05425e5e10df91e0
Mehul Gandhi <mehul.bigfaida@gmail.com>
2026-04-11 17:28:48 +0000

    fix: disable worker entry in API container

--- src/magicreel/worker-entry.ts
+++ src/magicreel/worker-entry.ts
@@ -1,3 +1,8 @@
+if (process.env.DISABLE_WORKER === "true") {
+  console.log("🚫 Worker entry disabled");
+  process.exit(0);
+}
+
 import "./workers/lookbookImage.worker";
 
 console.log("🧠 Lookbook worker process started");
